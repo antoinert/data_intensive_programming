cded99a36ef1d33276f476eea6fb60e0c034e033
antonrautio <anton.rautio@insa-lyon.fr>
2017-09-08 13:52:19 +0300

    task2 done

--- viikkoharkat/.worksheet/src/ex1.scala
+++ viikkoharkat/.worksheet/src/ex1.scala
@@ -4,6 +4,21 @@ object ex1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; d
 		x+y
   };System.out.println("""sum: (x: Int, y: Int)Int""");$skip(25); 
   
-  val s = sum(20, 21);System.out.println("""s  : Int = """ + $show(s ))}
+  val s = sum(20, 21);System.out.println("""s  : Int = """ + $show(s ));$skip(403); 
  
- }
+ 
+ //TASK 2
+ 	def sqrt(root: Double): Double = {
+ 		val initEst: Double = 1
+ 		val initMean: Double = (initEst+root)/2
+ 		val initQuo: Double = (root/initEst)
+ 		
+ 		def sqrtInit(r: Int = 300, estimation: Double, quotient: Double, mean: Double): Double = {
+ 			if (r == 0) mean
+ 			else sqrtInit(r-1, mean, root/mean, (estimation+quotient)/2)
+ 		}
+ 		
+ 		sqrtInit(300, initEst, initQuo, initMean)
+ 	};System.out.println("""sqrt: (root: Double)Double""");$skip(9); val res$0 = 
+	sqrt(2);System.out.println("""res0: Double = """ + $show(res$0))}
+}
--- viikkoharkat/ex1/ex1.sc
+++ viikkoharkat/ex1/ex1.sc
@@ -6,4 +6,19 @@ object ex1 {
   
   val s = sum(20, 21)                             //> s  : Int = 41
  
- }
+ 
+ //TASK 2
+ 	def sqrt(root: Double): Double = {
+ 		val initEst: Double = 1
+ 		val initMean: Double = (initEst+root)/2
+ 		val initQuo: Double = (root/initEst)
+ 		
+ 		def sqrtInit(r: Int = 300, estimation: Double, quotient: Double, mean: Double): Double = {
+ 			if (r == 0) mean
+ 			else sqrtInit(r-1, mean, root/mean, (estimation+quotient)/2)
+ 		}
+ 		
+ 		sqrtInit(300, initEst, initQuo, initMean)
+ 	}                                         //> sqrt: (root: Double)Double
+	sqrt(2)                                   //> res0: Double = 1.414213562373095
+}
